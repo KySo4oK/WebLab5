05f9c5577840c9efb82954ddbb35edb41d243317
KySo4oK <prostorostuk08@gmail.com>
2019-10-31 22:51:50 +0200

    edit index.js(actually wrote)

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-Date.prototype.getWeek = function() {
-    let oneJan = new Date(this.getFullYear(),0,1);
-    return Math.ceil((((this - oneJan) / 86400000) + oneJan.getDay()+1)/7);
+const http = require('http')
+const port = 3000
+const requestHandler = (request, response) => {
+    console.log(request.url)
+    response.end('Hello Node.js Server!')
 }
-let today = new Date();
-let weekNumber = today.getWeek();
-console.log(weekNumber);
+const server = http.createServer(requestHandler)
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    Date.prototype.getWeek = function() {
+        let firstJanuary = new Date(this.getFullYear(),0,1);
+        return Math.ceil((((this - firstJanuary) / 86400000) + firstJanuary.getDay()+1)/7);
+    }
+    let today = new Date(2019,0,6);
+    let weekNumber = today.getWeek();
+    alert(weekNumber);
+    console.log(`server is listening on ${port}`)
+})
