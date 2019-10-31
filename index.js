98794556078e56d51020d6afdfb34ecfb362de1c
KySo4oK <prostorostuk08@gmail.com>
2019-10-31 22:58:10 +0200

    rewrite index.js

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-const http = require('http')
-const port = 3000
-const requestHandler = (request, response) => {
-    console.log(request.url)
-    response.end('Hello Node.js Server!')
+let express = require('express');
+let app = express();
+
+app.get('/', function (req, res) {
+    res.send(weekNumber);
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+Date.prototype.getWeek = function() {
+    let firstJanuary = new Date(this.getFullYear(),0,1);
+    return Math.ceil((((this - firstJanuary) / 86400000) + firstJanuary.getDay()+1)/7);
 }
-const server = http.createServer(requestHandler)
-server.listen(port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err)
-    }
-    Date.prototype.getWeek = function() {
-        let firstJanuary = new Date(this.getFullYear(),0,1);
-        return Math.ceil((((this - firstJanuary) / 86400000) + firstJanuary.getDay()+1)/7);
-    }
-    let today = new Date(2019,0,6);
-    let weekNumber = today.getWeek();
-    alert(weekNumber);
-    console.log(`server is listening on ${port}`)
-})
+let today = new Date(2019,0,6);
+let weekNumber = today.getWeek();
