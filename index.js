b4a9eba823a916b6f6779a0698d06142127a25f6
KySo4oK <prostorostuk08@gmail.com>
2019-11-01 00:44:53 +0200

    use right index.js

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ let express = require('express');
 let app = express();
 
 app.get('/', function (req, res) {
-    res.send(weekNumber);
+
+    res.sendStatus(f());
 });
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
-Date.prototype.getWeek = function() {
-    let firstJanuary = new Date(this.getFullYear(),0,1);
-    return Math.ceil((((this - firstJanuary) / 86400000) + firstJanuary.getDay()+1)/7);
+function f() {
+    let r =10;
+    return Math.PI*r*r;
 }
-let today = new Date(2019,0,6);
-let weekNumber = today.getWeek();
+
