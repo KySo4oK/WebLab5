59ad5be0c3a92d0bff9fafa2bc1451ccc8c88043
KySo4oK <prostorostuk08@gmail.com>
2019-11-01 00:34:09 +0200

    Revert "rewrite index.js again"
    This reverts commit 096673d501cf4ff1295c0dc656d0331c0b6c0873.

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ let express = require('express');
 let app = express();
 
 app.get('/', function (req, res) {
-
-    res.sendStatus(f());
+    res.send(weekNumber);
 });
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
-function f() {
-        let r =10;
-        return Math.PI*r*r;
+Date.prototype.getWeek = function() {
+    let firstJanuary = new Date(this.getFullYear(),0,1);
+    return Math.ceil((((this - firstJanuary) / 86400000) + firstJanuary.getDay()+1)/7);
 }
+let today = new Date(2019,0,6);
+let weekNumber = today.getWeek();
