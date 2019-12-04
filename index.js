8903feef47cc1381654955f09544a77c758d30c8
KySo4oK <prostorostuk08@gmail.com>
2019-12-05 01:29:37 +0200

    add simple html with routing

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 let express = require('express');
 let app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-    res.sendStatus(f());
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/about', function (req, res) {
+    res.sendFile(__dirname + '/about.html');
 });
 
 app.listen(PORT, function () {
     console.log('Example app listening on port 3000!');
 });
-function f() {
-    let r =20;
-    return Math.PI*r*r;
-}
+
 
