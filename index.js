a3363186f991e4cd13247fa0c2e58ef2377c03b2
KySo4oK <prostorostuk08@gmail.com>
2019-12-05 23:50:15 +0200

    trying get data from .json and routing pages

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let express = require('express');
 let app = express();
+let fs = require('fs');
+
 const PORT = process.env.PORT || 3000;
+let json = fs.readFileSync('students.json', 'utf8');
+let students = JSON.parse(json);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -14,12 +18,22 @@ app.get('/about', function (req, res) {
 });
 
 app.get('/students', function (req, res) {
-    let students = [{name:'name1'},{name:'name2'},{name:'name3'}];
-    res.render('students', {array : students});
-})
+        res.render('students', {array : students});
+});
+
+app.get('/student/:id', function (req, res) {
+    let id = parseInt(req.params.id);
+    console.log(students.students[id]);
+    res.render('student', {student : students[id]});
+});
 
 app.listen(PORT, function () {
     console.log('Example app listening on port 3000!');
 });
+
+// function addStudent(student) {
+//     students.push(student);
+//     fs.writeFileSync('students.json', students.stringify);
+// }
 
 
