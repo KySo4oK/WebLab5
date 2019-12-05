31d3beef21e51a9ebae762aef867adef7ca1149d
KySo4oK <prostorostuk08@gmail.com>
2019-12-06 01:36:29 +0200

    wrote read and write .json with routing

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 let express = require('express');
 let app = express();
 let fs = require('fs');
+let bodyParser = require('body-parser');
 
+let urlencodedParser = bodyParser.urlencoded({extended: false});
 const PORT = process.env.PORT || 3000;
+
 let json = fs.readFileSync('students.json', 'utf8');
 let students = JSON.parse(json);
 
@@ -18,22 +21,36 @@ app.get('/about', function (req, res) {
 });
 
 app.get('/students', function (req, res) {
-        res.render('students', {array : students});
+    res.render('students', {array: students});
 });
 
 app.get('/student/:id', function (req, res) {
     let id = parseInt(req.params.id);
-    console.log(students.students[id]);
-    res.render('student', {student : students[id]});
+    res.render('student', {student: students.students[id]});
+});
+
+app.post('/add', urlencodedParser, function (req, res) {
+    let newStudent =
+        {
+            "first" : req.body.first,
+            "last" : req.body.last
+    };
+    addStudent(newStudent);
+    res.render('add');
+});
+
+app.get('/add', urlencodedParser, function (req, res) {
+    res.render('add');
 });
 
 app.listen(PORT, function () {
     console.log('Example app listening on port 3000!');
 });
 
-// function addStudent(student) {
-//     students.push(student);
-//     fs.writeFileSync('students.json', students.stringify);
-// }
+
+function addStudent(student) {
+    students.students.push(student);
+    fs.writeFileSync('students.json', JSON.stringify(students));
+}
 
 
