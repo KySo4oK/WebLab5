b848aba8a928673f05ff34d26c3ab4c32c5c75f9
KySo4oK <prostorostuk08@gmail.com>
2019-12-05 02:16:47 +0200

    add ejs template

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ let express = require('express');
 let app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
@@ -9,6 +12,11 @@ app.get('/', function (req, res) {
 app.get('/about', function (req, res) {
     res.sendFile(__dirname + '/about.html');
 });
+
+app.get('/students', function (req, res) {
+    let students = [{name:'name1'},{name:'name2'},{name:'name3'}];
+    res.render('students', {array : students});
+})
 
 app.listen(PORT, function () {
     console.log('Example app listening on port 3000!');
